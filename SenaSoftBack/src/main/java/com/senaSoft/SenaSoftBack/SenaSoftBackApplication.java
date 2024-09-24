1e8a261389a1702f266b3235e71216d6da69af92
JonathanE2607 <159081123+JonathanE2607@users.noreply.github.com>
2024-09-24 11:41:35 -0500

    first commit

---
+++ SenaSoftBack/src/main/java/com/senaSoft/SenaSoftBack/SenaSoftBackApplication.java
@@ -0,0 +1,13 @@
+package com.senaSoft.SenaSoftBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenaSoftBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenaSoftBackApplication.class, args);
+	}
+
+}
---
+++ SenaSoftBack/src/test/java/com/senaSoft/SenaSoftBack/SenaSoftBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senaSoft.SenaSoftBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenaSoftBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
